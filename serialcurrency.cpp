a2172847afe5324574315fa99dbd50aea06903bc
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2023-01-22 17:06:34 -0300

    implementacao de serialcurrency

---
+++ serialcurrency.cpp
@@ -0,0 +1,138 @@
+#include "serialcurrency.h"
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+serialcurrency::serialcurrency() : serializable(){ // Construtor padrão
+    intPart = 0;
+    fracPart = 0;
+}
+
+serialcurrency::serialcurrency(double value) : serializable(){ // Construtor paramétrico
+    double aux;
+
+    aux = modf(value, &intPart);
+
+    setFracPart(aux);
+}
+
+serialcurrency::serialcurrency(const serialcurrency& other){ // Construtor de cópia
+    intPart = other.getIntPart();
+    setFracPart(other.getFracPart());
+}
+
+serialcurrency::~serialcurrency(){ // Destrutor
+
+}
+
+serialcurrency serialcurrency:::operator=(const serialcurrency& other){ // Sobrecarga do operador =
+    serialCurrency aux(other);
+
+    if(this == &other){
+        return *this;
+    }
+
+    intPart = other.getIntPart();
+    setFracPart(other.getFracPart());
+
+    return aux;
+}
+
+bool serialcurrency::operator==(const serialcurrency &other) const{ // Sobrecarga do operador ==
+    if(getIntPart() != other.getIntPart()){
+        return false;
+    } else{
+        return getFracPart() == other.getFracPart();
+    }
+}
+
+bool serialcurrency::operator<(const serialcurrency &other) const{ // Sobrecarga do operador <
+    if(getIntPart() != other.getIntPart()){
+        return getIntPart() < other.getIntPart() && getFracPart() < other.getFracPart();
+    } else{
+        return getFracPart() < other.getFracPart();
+    }
+}
+
+bool serialcurrency::operator<= (const serialcurrency &other) const{ // Sobrecarga do operador <=
+    if(getIntPart() <= other.getIntPart()){
+        return getFracPart() <= other.getFracPart();
+    } else{
+        return false;
+    }
+}
+
+bool serialcurrency::operator> (const serialcurrency &other) const{
+    if(getIntPart() != other.getIntPart()){
+        return getIntPart() > other.getIntPart() && getFracPart() > other.getFracPart();
+    } else{
+        return getFracPart() > other.getFracPart();
+    }
+}
+
+bool serialcurrency::operator>= (const serialcurrency &other) const{
+    if(getIntPart() >= other.getIntPart()){
+        return getFracPart() >= other.getFracPart();
+    } else{
+        return false;
+    }
+}
+
+bool serialcurrency::operator!=(const serialcurrency &other) const{
+    if(getIntPart() != other.getIntPart()){
+        return true;
+    } else{
+        return getFracPart() != other.getFracPart();
+    }
+}
+
+friend ostream& operator<<(ostream& os, serialcurrency &sc){
+    os << fixed << setprecision(2) << sc.intPart + sc.fracPart;
+    return os;
+}
+
+void serialcurrency::setIntPart(double newIntPart){ // Modificador do atributo intPart
+    intPart = newIntPart;
+}
+
+double serialcurrency::getIntPart() const{ // Acessor do atributo intPart
+    return intPart;
+}
+
+void serialcurrency::setFracPart(double newFracPart){; // Modificador do atributo fracPart
+    if(newFracPart >= 0.100){
+        double hundreds;
+        hundreds = newFracPart/100;
+        intPart = intPart + int(hundreds);
+        fracPart = newFracPart - int(hundreds)*100;
+    } else{
+        fracPart = getFracPart() + newFracPart;
+    }
+}
+
+double serialcurrency::getFracPart() const{
+    return fracPart;
+}
+
+string serialcurrency::toString(){ // Serialização
+    string repr = "";
+
+    repr += string(reintepret_cast<char*>(&intPart). sizeof(intPart));
+    repr += string(reinterpret_cast<char*>(&fracPart), sizeof(fracPart));
+
+    return repr;
+}
+
+void serialcurrency::fromString(){ // Serialização
+    int pos = 0;
+
+    repr.copy(reintepret_cast<char*>(&intPart), sizeof(intPart), pos);
+    pos += sizeof(intPart);
+    repr.copy(reinterpret_cast<char*>(&fracPart), sizeof(fracPart), pos);
+}
+
+unsigned long long int serialcurrency::size() const{ // Calcula o tamanho de um objeto da classe serialCurrency
+    return sizeof(intPart) + sizeof(fracPart);
+}
---
+++ serialcurrency.h
@@ -0,0 +1,40 @@
+#ifndef SERIALCURRENCY_H_INCLUDED
+#define SERIALCURRENCY_H_INCLUDED
+
+#include "serializable.h"
+
+class serialcurrency : serializable{
+    protected:
+        double intPart; // intPart é o atributo que refere-se a parte inteira do valor
+        double fracPart; // fracPart é o atributo que refere-se a parte fracionária do valor
+    public:
+        serialcurrency(); // Construtor padrão
+        serialcurency(double value); // Construtor paramétrico
+        serialcurrency(const serialcurrency& other); // Construtor de cópia
+        virtual ~serialcurrency(); // Destrutor
+        serialcurrency operator=(const serialcurrency& other); // Sobrecarga do operador =
+        bool operator==(const serialcurrency &other) const; // Sobrecarga do operador ==
+        bool operator<(const serialcurency &other) const; // Sobrecarga do operador <
+        bool operator<=(const serialcurrency &other) const; // Sobrecarga do operador <=
+        bool operator>(const serialcurrency &other) const; // Sobrecarga do operador >
+        bool operator>=(const serialcurrency &other) const; // Sobrecarga do operador >=
+        bool operator!=(const serialcurrency &other) const; // Sobrecarga do operador !=
+        void setIntPart(double newIntPart); // Modificador do atributo x
+        double getIntPart() const; // Acessor do atributo x
+        void setFracPart(double newFracPart); // Modificador do atributo y
+        float getFracPart() const; // Acessor do atributo y
+        virtual string toString(); // Serialização
+        virtual void fromString(string repr); // Desserialização
+        friend ostream& operator<<(ostream& os, serialcurrency &sc); // Sobrecarga do operador <<
+        // *** Não implementados
+        virtual string toXML(){ return ""; };
+        virtual void fromXML(string repr){};
+        virtual string toCSV(){ return ""; };
+        virtual void fromCSV(string repr){};
+        virtual string toJSON(){ return ""; };
+        virtual void fromJSON(string repr){};
+        // ***
+        virtual unsigned long long int size() const; // Método que calcula o tamanho do objeto da classe serialCurrency
+};
+
+#endif // SERIALCURRENCY_H_INCLUDED
